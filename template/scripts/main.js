81e7a918e8803767f5bfcfe129f77cbf71314b1d
cafemocha0804 <moka_tabata@net93-dhcp-43.sfc.keio.ac.jp>
2016-11-03 12:59:28 +0900

    中間テストの練習問題の解説

---
+++ template/scripts/main.js
@@ -0,0 +1,40 @@
+function id(value){
+  return value;
+}
+
+
+function classifyInt(integer){
+    let amari = integer % 2;
+   if(amari == 0){
+     return "偶数";
+   }else{
+     return "奇数";  
+   }
+}
+
+
+function gcd(integerA, integerB){
+  let result = 1;
+  let i = 2;
+  let end = Math.min(integerA, integerB);
+  while(i <= end){
+    let amariA = integerA % i ; 
+    let amariB = integerB % i ;  
+    if (amariA == 0 && amariB == 0); 
+       result = i;
+    }
+    i = i + 1;
+  }
+ return result;
+}
+
+
+function sum(lowerBound, upperBound){
+   let result =0;
+   let i = Math.ceil(lowerBound);
+   while(i <= upperBound){
+     result = result + i;
+     i = i + 1;
+   }
+return result;
+}
